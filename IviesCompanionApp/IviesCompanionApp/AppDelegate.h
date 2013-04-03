c8c041393d812aaa83da6c46babeed2e6314fc95
Connor Smith <cmds4410@gmail.com>
2013-04-03 14:03:27 -0400

    Added Xcode Project

---
+++ IviesCompanionApp/IviesCompanionApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IviesCompanionApp
+//
+//  Created by Connor Smith on 4/3/13.
+//  Copyright (c) 2013 Connor Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ IviesCompanionApp/IviesCompanionApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IviesCompanionApp
+//
+//  Created by Connor Smith on 4/3/13.
+//  Copyright (c) 2013 Connor Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
