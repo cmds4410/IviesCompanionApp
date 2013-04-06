7f0204fb288d70dc56b0714d535e94116444a692
Connor Smith <cmds4410@gmail.com>
2013-04-06 00:40:32 -0400

    Added ScrollView to PictureViewController

--- IviesCompanionApp/IviesCompanionApp/PictureViewController.m
+++ IviesCompanionApp/IviesCompanionApp/PictureViewController.m
@@ -65,13 +65,38 @@ const float WATERMARK_ALPHA = .75;
     if (buttonIndex == 0 && [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
     {
         picker.sourceType = UIImagePickerControllerSourceTypeCamera;
-        [self presentViewController:picker animated:YES completion:nil];
-        // This block of code is only needed in case you want your watermark to be displayed also during the shooting process
-        UIImageView *anImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"overlay1"]];
-        anImageView.alpha = WATERMARK_ALPHA;
-        anImageView.contentMode = UIViewContentModeTopLeft;
-        anImageView.frame = picker.view.frame;
-        picker.cameraOverlayView = anImageView;
+        [self presentViewController:picker animated:YES completion:^{
+            // This block of code is only needed in case you want your watermark to be displayed also during the shooting process
+            UIImageView *anImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"overlay1"]];
+            anImageView.alpha = WATERMARK_ALPHA;
+            anImageView.contentMode = UIViewContentModeScaleAspectFit;
+            
+            anImageView.frame = CGRectMake(0, 20, self.view.bounds.size.width, 100);
+            
+            UIScrollView* scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 100)];
+            scrollView.pagingEnabled = YES;
+            int numberOfViews = 3;
+            for (int i = 0; i < numberOfViews; i++)
+            {
+                CGFloat xOrigin = i * self.view.frame.size.width;
+                UIView *awesomeView = [[UIView alloc] initWithFrame:CGRectMake(xOrigin, 0, self.view.frame.size.width, self.view.frame.size.height)];
+                //awesomeView.backgroundColor = [UIColor colorWithRed:0.5/i green:0.5 blue:0.5 alpha:1];
+                UIImageView *anImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"overlay1"]];
+                anImageView.alpha = WATERMARK_ALPHA;
+                anImageView.contentMode = UIViewContentModeScaleAspectFit;
+                
+                anImageView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 100);;
+                [awesomeView addSubview:anImageView];
+                
+                [scrollView addSubview:awesomeView];
+            }
+            scrollView.contentSize = CGSizeMake(self.view.frame.size.width * numberOfViews, scrollView.frame.size.height);
+            //        scrollView.showsHorizontalScrollIndicator = NO;
+            
+            picker.cameraOverlayView = scrollView;
+            
+            [scrollView flashScrollIndicators];
+        }];
         
         
     }
@@ -96,11 +121,13 @@ const float WATERMARK_ALPHA = .75;
 #pragma - mark UIImagePickerControllerDelegate
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    UIGraphicsBeginImageContext(CGSizeMake(320, 480));
+//    UIGraphicsBeginImageContext(CGSizeMake(320, 480));
+    UIGraphicsBeginImageContext(CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height));
+
     // This is where we resize captured image
-    [(UIImage *)[info objectForKey:UIImagePickerControllerOriginalImage] drawInRect:CGRectMake(0, 0, 320, 480)];
+    [(UIImage *)[info objectForKey:UIImagePickerControllerOriginalImage] drawInRect:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
     // And add the watermark on top of it
-    [[UIImage imageNamed:@"overlay1.png"] drawInRect:CGRectMake(0, 0, 320, 100) blendMode:kCGBlendModeNormal alpha:WATERMARK_ALPHA];
+    [[UIImage imageNamed:@"overlay1.png"] drawInRect:CGRectMake(0, 0, self.view.bounds.size.width, 100) blendMode:kCGBlendModeNormal alpha:WATERMARK_ALPHA];
     // Save the results directly to the image view property
     self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
